dc2d60f1cb42cd529f30a7d0d5370315c0b243b3
vjgaur <vijayendra.gaur@gmail.com>
2021-09-15 18:07:23 +0530

    added db server and connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -17,4 +24,10 @@ app.get('/api/products/:id', (req,res)=> {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
-app.listen(5000, console.log('Server running on port 5000'))
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline)
+)
