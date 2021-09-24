d03d4a4135db41603e41e56a57dd1131195c2185
vjgaur <vijayendra.gaur@gmail.com>
2021-09-24 16:02:31 +0530

    added spinner from react-bootstrap and error message formatting

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { spinner } from 'react-bootstrap'
+
+const Loader = () => {
+
+    return (
+        <spinner
+            animation='border'
+            role='satus'
+            style={{
+                width: '100px',
+                height: '100px',
+                margin: 'auto',
+                display: 'block',
+            }}
+        >
+            <spanm class='sr-only'>Loading...</spanm>
+        </spinner>
+    )
+}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
@@ -18,8 +20,8 @@ const HomeScreen = () => {
     return (
         <>
             <h1> Latest Products</h1>
-            {loading ? <h2>Loading... </h2>
-                : error ? <h3>{error}</h3> :
+            {loading ? <Loader/>
+                : error ? <Message variant= 'danger'>{error}</Message> :
             <Row>
             {products.map(product => (
 
